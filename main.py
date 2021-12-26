fcaf7a2559eb5eb4124d641d1d76b45c76605ae1
SVS1170 <44399077+SVS1170@users.noreply.github.com>
2021-12-27 01:09:56 +0300

    OPTIMIZE

--- main.py
+++ main.py
@@ -13,65 +13,6 @@ st_time = str(start_time.strftime("%Y%m%d%H%M%S"))
 tarif = 4.96
 
 
-# stream_id формат (int)
-# start_time формат (timestamp)
-
-# def ret_start_end_time(stream_id, start_time):
-#     #start_time = int(start_time)
-#     #start_time = datetime.fromtimestamp(start_time, tz=timezone.utc)
-#     #st_time = start_time
-#     st_time = start_time.strftime("%Y-%m-%d:%H-%M-%S")
-#     #print(st_time)
-#     config = configparser.ConfigParser()
-#     config.read("config.ini")  # читаем конфиг
-#     # создаём подключение с помощью метода Connect
-#     db = pymysql.connect(host=config["database"]["host"],           # your host, usually localhost
-#                          port=config["database"]["port"],                     #your port
-#                          user=config["database"]["user"],           # your username
-#                          passwd=config["database"]["passwd"],  # your password
-#                          db=config["database"]["db"])           # name of the data base
-#     # создаём объект db с помощью метода cursor() модуля MySQLdb
-#     cursor = db.cursor()
-#
-#     # получить channel_id из базы по stream_id:
-#     epg_id = f"select channel_id from streams WHERE id={stream_id}"
-#     cursor.execute(epg_id)
-#     chanel_id = cursor.fetchall()
-#     ch_id = chanel_id[0][0]
-#     #print(ch_id)
-#
-#     # получить время начала телепередачи из базы по channel_id:
-#     strt_time = f'select start from epg_data where channel_id={ch_id} ORDER BY ABS( TIMEDIFF( start, "{st_time}")) limit 1;'
-#     cursor.execute(strt_time)
-#     s_time = cursor.fetchall()
-#     #print(s_time[0][0].strftime("%Y-%m-%d:%H-%M"))
-#
-#     #пример извлечения данных из tuple datetime по маске:
-#     #a = [(datetime(2012, 10, 21, 13, 56, 24),)]
-#     #print(a[0][0].strftime("%Y-%m-%d:%H-%M-%S"))
-#
-#     # получить время конца телепередачи из базы по channel_id:
-#     end_time = f'select end from epg_data where channel_id={ch_id} ORDER BY ABS( TIMEDIFF( start, "{st_time}" )) limit 1;'
-#     cursor.execute(end_time)
-#     e_time = cursor.fetchall()
-#     #print(e_time[0][0].strftime("%Y-%m-%d:%H-%M"))
-#
-#     # получить длительноть телепередачи:
-#     fmt = '%Y-%m-%d %H:%M'
-#     d1 = s_time[0][0]
-#     d2 = e_time[0][0]
-#     # #d1 = s_time
-#     # #d2 = e_time
-#     diff = d2-d1
-#     #print(diff.days * 24 * 60 + (diff.seconds/60))
-#     diffminutes = diff.days * 24 * 60 + (diff.seconds/60)
-#     # и отключаемся от сервера
-#     db.close()
-#     return[(s_time[0][0].strftime("%Y-%m-%d:%H-%M")),(s_time[0][0]), (e_time[0][0].strftime("%Y-%m-%d:%H-%M")), diffminutes]
-#
-# #print(ret_start_end_time(stream_id, start_time))
-
-
 def read(name):
     config = configparser.ConfigParser()
     config.read("config.ini")  # читаем конфиг
